9d0bcd86bf2e459c03163085b1f290f72c7347f8
ashmoune <146857243+ashmoune@users.noreply.github.com>
2024-04-18 23:30:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// import des packages
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const axios = require("axios");
+// const cloudinary = require("cloudinary").v2;
+
+const app = express();
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+
+const locationsRoutes = require("./routes/locations");
+app.use(locationsRoutes);
+
+app.get("/", (req, res) => {
+  try {
+    return res.status(200).json({ message: "Bienvenue sur notre serveur" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  return res.status(404).json("Not found");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is 🧨🧨🧨🧨 on ${process.env.PORT}`);
+});
---
+++ routes/locations.js
@@ -0,0 +1,88 @@
+const axios = require("axios");
+const express = require("express");
+const router = express.Router();
+
+router.get("/locations", async (req, res) => {
+  const { text } = req.query;
+
+  if (!text) {
+    return res.status(400).json({ error: "text parameter is required" });
+  }
+
+  try {
+    //  1: on utilise  /locations/v2/auto-complete pour obtenir des suggestions
+    const autoCompleteResponse = await axios.get(
+      "https://the-fork-the-spoon.p.rapidapi.com/locations/v2/auto-complete",
+      {
+        params: { text },
+        headers: {
+          "X-RapidAPI-Key": process.env.XRAPIDAPIKEY,
+          "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+        },
+      }
+    );
+
+    // vérifie si des suggestions sont retournées
+    if (
+      autoCompleteResponse.data &&
+      autoCompleteResponse.data.data &&
+      autoCompleteResponse.data.data.geolocation &&
+      autoCompleteResponse.data.data.geolocation.length > 0
+    ) {
+      //  le premier objet de géolocalisation
+      const geolocation = autoCompleteResponse.data.data.geolocation[0];
+      const googlePlaceId = geolocation.id.id;
+      console.log("Google Place ID:", googlePlaceId);
+
+      //  2: on utilise  /locations/v2/list pour obtenir des lieux associés à l'ID de lieu Google
+      const listResponse = await axios.get(
+        "https://the-fork-the-spoon.p.rapidapi.com/locations/v2/list",
+        {
+          params: {
+            google_place_id: googlePlaceId,
+            geo_ref: "false",
+            geo_text: text,
+            geo_type: geolocation.type,
+          },
+          headers: {
+            "X-RapidAPI-Key": process.env.XRAPIDAPIKEY,
+            "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+          },
+        }
+      );
+      console.log("List Response:", listResponse.data);
+
+      //
+      if (listResponse.data && listResponse.data.id_city) {
+        // ID de la ville à partir de la réponse de la liste
+        const cityId = listResponse.data.id_city;
+        console.log("City ID:", cityId);
+
+        //  3: on utilise /restaurants/v2/list pour obtenir la liste des restaurants
+        const restaurantsResponse = await axios.get(
+          "https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list",
+          {
+            params: { queryPlaceValueCityId: cityId },
+            headers: {
+              "X-RapidAPI-Key": process.env.XRAPIDAPIKEY,
+              "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+            },
+          }
+        );
+
+        // renvoie la liste des restaurants dans la réponse
+        res.json({ restaurants: restaurantsResponse.data });
+      } else {
+        res
+          .status(404)
+          .json({ error: "No locations found for the given text" });
+      }
+    } else {
+      res.status(404).json({ error: "No locations found for the given text" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
