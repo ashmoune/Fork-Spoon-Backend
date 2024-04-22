43586e0cfff4d0b73cf5502edd0ee27796fbb077
ashmoune <146857243+ashmoune@users.noreply.github.com>
2024-04-22 23:34:55 +0200

    added restaurantid route

--- routes/locations.js
+++ routes/locations.js
@@ -85,4 +85,33 @@ router.get("/locations", async (req, res) => {
   }
 });
 
+router.get(`/restaurants/:id`, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const restaurantResponse = await axios.get(
+      `https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/get-info/`,
+      {
+        params: {
+          restaurantId: id,
+        },
+        headers: {
+          "X-RapidAPI-Key": process.env.XRAPIDAPIKEY,
+          "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+        },
+      }
+    );
+
+    if (restaurantResponse.data) {
+      res.json({ restaurant: restaurantResponse.data });
+      console.log(restaurantResponse.data);
+    } else {
+      res.status(404).json({ error: "Restaurant not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
